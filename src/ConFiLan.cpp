d22dfdb00c9baa0c99294bf6e6eb62fd96922ded
nikozdev <nikozdev@nikozdev.net>
2023-08-03 19:53:57 -0700

    !env!define!preproc!main!test!class!type!process!export!import
    [env/CMakeLists.txt] > added preprocessor definitions for tools
    [src/ConFiLan.hpp]
    > declared the scope class
    > writing dll export-import macros
    [src/ConFiLan.cpp]
    > added some helper macros
    > implementing the scope class

--- src/ConFiLan.cpp
+++ src/ConFiLan.cpp
@@ -1,15 +1,105 @@
 #ifndef dConFiLanCpp
 #define dConFiLanCpp
+//headers
 #include "ConFiLan.hpp"
+//-//external
 #include "dTermAPar.dir/fHead.hxx"
 #include "boost/filesystem.hpp"
+#include "boost/filesystem/fstream.hpp"
+//-//standard
 #include <cstddef>
 #include <cstdlib>
 #include <algorithm>
+//defines
+#define fActIfEqu(vExpr, vBool, ...) \
+	({                                 \
+		if((vExpr) == vBool)             \
+		{                                \
+			__VA_ARGS__;                   \
+		}                                \
+	})
+#define fActIfNot(vExpr, ...) fActIfEqu(vExpr, 0, __VA_ARGS__);
+#define fActIfYes(vExpr, ...) fActIfEqu(vExpr, 1, __VA_ARGS__);
+//content
+namespace nConFiLan
+{
+//typedef
+using tPath = boost::filesystem::path;
+//setters
+auto tScope::fSet(const tIndex &rIndex, const tValue &rValue) -> tScope *
+{
+	this->vTable[rIndex] = std::make_shared<tValue>(rValue);
+	return this;
+}
+auto tScope::fSet(const tIndex &rAlias, const tIndex &rIndex) -> tScope *
+{
+	this->vTable[rAlias] = this->vTable[rIndex];
+	return this;
+}
+//getters
+auto tScope::fGet(const tIndex &rIndex) const -> std::optional<tRefer>
+{
+	auto vFound = this->vTable.find(rIndex);
+	auto vEmpty = vFound == this->vTable.end();
+	return vEmpty ? std::optional<tRefer>() : vFound->second;
+}
+//vetters
+bool tScope::fVet(const tIndex &rIndex)
+{
+	auto vFound = this->vTable.find(rIndex);
+	auto vEmpty = vFound == this->vTable.end();
+	return vEmpty;
+}
+//actions
+auto tScope::fMake(const tData &rData) -> tScope *
+{
+	return this;
+}
+template<>
+auto tScope::fLoad<tPath>(const tPath &rPath) -> tScope *
+{
+	//file
+	auto vFile = boost::filesystem::ifstream(rPath);
+	fActIfNot(vFile.is_open(), throw std::logic_error(rPath.c_str()));
+	//size
+	vFile.seekg(0, std::ios::end);
+	auto vSize = tSize(vFile.tellg());
+	vFile.seekg(0, std::ios::beg);
+	//data
+	auto vData = tData(vSize, '\0');
+	while(vFile >> vData)
+	{
+	}
+	//quit
+	return this->fMake(vData);
+}
+template<>
+auto tScope::fSave<tPath>(const tPath &rPath) -> tScope *
+{
+	//file
+	auto vFile = boost::filesystem::ofstream(rPath);
+	fActIfNot(vFile.is_open(), throw std::logic_error(rPath.c_str()));
+	//quit
+	return this;
+}
+//operats
+template<>
+auto tScope::operator<<(std::ostream &rStream) -> std::ostream &
+{
+	return rStream;
+}
+template<>
+auto tScope::operator>>(std::istream &rStream) -> std::istream &
+{
+	return rStream;
+}
+}//namespace nConFiLan
+#ifdef dConFiLanMakeExe
 namespace nConFiLan
 {
 int fMain(int vArgC, char **vArgV)
 {
+	boost::filesystem::ifstream v;
 	boost::filesystem::current_path(dConFiLanPathToInternal);
 	nTermAPar::tArgParser vArgParser;
 	vArgParser.fSetCmd(
@@ -36,7 +126,6 @@ int fMain(int vArgC, char **vArgV)
 	return EXIT_SUCCESS;
 }
 }//namespace nConFiLan
-#ifdef dConFiLanMakeExe
 //actions
 int main(int vArgC, char **vArgV)
 {
--- src/ConFiLan.hpp
+++ src/ConFiLan.hpp
@@ -1,6 +1,94 @@
 #ifndef dConFiLanHpp
 #define dConFiLanHpp
+//headers
+#include <string>
+#include <memory>
+#include <map>
+#include <variant>
+#include <optional>
+//defines
+#if defined(dConFiLanMakeDll)
+#if defined(dConFiLanToolMsvcc)
+#define aConFiLanExternal __declspec(dllexport)
+#define aConFiLanInternal ()
+#endif//dConFiLanToolMsvcc
+#if defined(dConFiLanToolGnucc)
+#define aConFiLanExternal __attribute__((visibility("default")))
+#define aConFiLanInternal __attribute__((visibility("hidden")))
+#endif//ifd(dConFiLanToolGnucc)
+#else
+#define aConFiLanExternal
+#define aConFiLanInternal
+#endif//ifd(dConFiLanMakeDll)
+//content
 namespace nConFiLan
 {
+//typedef
+using tData = std::string;
+using tSize = size_t;
+/* type of scope
+ * container of index-value pairs and nested scopes
+ */
+class aConFiLanExternal tScope
+{
+public://typedef
+
+	using tLevel = size_t;
+	using tIndex = std::string;
+	using tValue = std::variant<std::string, tScope>;
+	using tRefer = std::shared_ptr<tValue>;
+	using tTable = std::map<tIndex, tRefer>;
+
+public://codetor
+
+	tScope(): vLevel{0}, vTable{}
+	{
+	}
+
+public://setters
+
+	auto fSet(const tIndex &rAlias, const tIndex &rIndex) -> tScope *;
+	auto fSet(const tIndex &rIndex, const tValue &rValue) -> tScope *;
+
+public://getters
+
+	auto fGet(const tIndex &rIndex) const -> std::optional<tRefer>;
+
+public://vetters
+
+	auto fVet(const tIndex &rIndex) -> bool;
+
+public://actions
+
+	auto fMake(const tData &rData) -> tScope *;
+
+	template<typename tPath>
+	auto fSave(const tPath &rPath) -> tScope *;
+	template<typename tPath>
+	auto fLoad(const tPath &rPath) -> tScope *;
+
+public://operats
+
+	template<typename tStream>
+	auto operator<<(tStream &rStream) -> tStream &;
+	template<typename tStream>
+	auto operator>>(tStream &rStream) -> tStream &;
+
+private://datadef
+
+	tLevel vLevel;
+	tTable vTable;
+
+};//tScope
+template<typename tStream>
+auto operator<<(tStream &rStream, tScope &rScope)
+{
+	return rScope.operator<<(rStream);
+}
+template<typename tStream>
+auto operator>>(tStream &rStream, tScope &rScope)
+{
+	return rScope.operator>>(rStream);
+}
 }//namespace nConFiLan
 #endif//dConFiLanHpp
